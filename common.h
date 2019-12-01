18b02eeb780a666dec95c3282b842ab62401aedd
Litjens <r.litjens@student.tue.nl>
2019-12-01 01:20:10 +0100

    farn

--- common.h
+++ common.h
@@ -14,8 +14,34 @@
 #define MAX_MESSAGE_LENGTH	6
  
 
-// TODO: put your definitions of the datastructures here
 
+
+//TODO:
+#define STUDENT_NAME        "Rik_Litjens"
+static char                 mq_name_jobs[80];
+static char                 mq_name_results[80];
+
+
+//extern uint128_t;
+/**
+ * Datastructures for the jobs and results that are 
+ * to be interchanged between the farmer and its workers 
+ */
+typedef struct
+{
+    // a data structure with 2 members
+    // start symbol s and hash h
+    char                    s;
+    uint128_t               h;
+} MQ_JOB;
+
+typedef struct
+{
+    // a data structure with 2 members
+    // result succes suc and msg m when succesfull 
+    int                     suc;
+    char                    m[6];
+} MQ_RESULT;
 
 #endif
 
--- farmer.c
+++ farmer.c
@@ -36,7 +36,84 @@ int main (int argc, char * argv[])
         
     // TODO:
     //  * create the message queues (see message_queue_test() in interprocess_basic.c)
+    /**
+     * Creating the message queues for sender jobs to workers
+     * and for receiving information from workers
+     */
+    pid_t               processID;      /* Process ID from fork() */
+    mqd_t               mq_fd_jobs;
+    mqd_t               mq_fd_results;
+    MQ_JOB  job;
+    MQ_RESULT result;
+    struct mq_attr      attr;
+
+    attr.mq_maxmsg  = MQ_MAX_MESSAGES;
+    attr.mq_msgsize = MAX_MESSAGE_LENGTH;
+    
+    sprintf (mq_fd_request, "/mq_request_%s_%d", STUDENT_NAME, getpid());
+    sprintf (mq_fd_result, "/mq_response_%s_%d", STUDENT_NAME, getpid());
+    
+    mq_fd_request = mq_open (mq_name_jobs, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);
+    mq_fd_result = mq_open (mq_name_results, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+    getattr(mq_fd_jobs);
+    getattr(mq_fd_results);
+
     //  * create the child processes (see process_test() and message_queue_test())
+    processID = fork();
+    if (processID < 0)
+    {
+        perror("fork() failed");
+        exit (1);
+    }
+    else
+    {
+        if (processID == 0)
+        {
+            // child-stuff
+            //message_queue_child ();
+            printf(processID);
+            exit (0);
+        }
+        else
+        {
+            // remaining of the parent stuff
+            printf(processID);
+            // fill job
+            // job.s = 73;
+            // job.h =   ;
+
+            // sleep (3);
+            // // send the request
+            // printf ("parent: sending...\n");
+            // mq_send (mq_fd_request, (char *) &job, sizeof (job), 0);
+
+            // sleep (3);
+            // // read the result and store it in the response message
+            // printf ("parent: receiving...\n");
+            // mq_receive (mq_fd_response, (char *) &rsp, sizeof (rsp), NULL);
+
+            // printf ("parent: received: %d, '", rsp.e);
+            // // printing characters of f[] separately:
+            // for (int i = 0; i < rsp.e; i++)
+            // {
+            //     printf ("%c", rsp.f[i]);
+            // }
+            // // printing g[] in one step (because it has the \0-terminator):
+            // printf ("', '%s'\n", rsp.g);
+    
+            // sleep (1);
+    
+            waitpid (processID, NULL, 0);   // wait for the child
+            
+            mq_close (mq_fd_results);
+            mq_close (mq_fd_jobs);
+            mq_unlink (mq_name_jobs);
+            mq_unlink (mq_name_results);   
+        }
+    }
+
+
     //  * do the farming
     //  * wait until the chilren have been stopped (see process_test())
     //  * clean up the message queues (see message_queue_test())
