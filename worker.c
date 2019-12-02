ab3c9a3e9b4773f9e47eef4ce47d0afb659d67db
Litjens <r.litjens@student.tue.nl>
2019-12-02 16:57:15 +0100

    sfsfsf

--- worker.c
+++ worker.c
@@ -44,24 +44,6 @@ getattr (mqd_t mq_fd)
                 mq_fd, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
 }
 
-
-
-static void tryPassword(char tryPsw[6], uint128_t h) {
-
-    /**
-     * Hash the password we are trying and compare it
-    */
-    printf("now we try hashing\n");
-    tryHash = md5s(tryPsw, strlen(tryPsw));
-    if(tryHash == job.h) {
-            printf("Jawel hoor ze zijn gelijk tis wat\n");
-            strncpy(result.m, tryPsw, sizeof(tryPsw));
-            result.h = job.h;
-            mq_send (mq_fd_results, (char *) &result, sizeof(result), 0);
-    }
-
-
-} 
 int main (int argc, char * argv[])
 {
     // TODO:
@@ -110,12 +92,23 @@ int main (int argc, char * argv[])
          */
         snprintf(tryPsw, sizeof(tryPsw), "%c", job.st);
         printf("Try: %s\n", tryPsw);
-        
-        
+
         rsleep(10000000);
+
+        /**
+         * Hash the password we are trying and compare it
+         */
+        printf("now we try hashing\n");
+        tryHash = md5s(tryPsw, strlen(tryPsw));
+
         printf("starts with '%c', %d %d\n", job.st, job.f, sizeof(job));
-        tryPasswords(tryPsw, job.h);
-        
+
+        if(tryHash == job.h) {
+            printf("Jawel hoor ze zijn gelijk tis wat\n");
+            strncpy(result.m, tryPsw, sizeof(tryPsw));
+            result.h = job.h;
+            mq_send (mq_fd_results, (char *) &result, sizeof(result), 0);
+        }
         
     }
     printf("EINDEEEEEE WHILEEEEEEEEEEE LOOOOOOOOOOOOP\n");
