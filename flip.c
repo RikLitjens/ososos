4d8d2a7826e3f33200e4738a78eb16839299ab91
Litjens <r.litjens@student.tue.nl>
2019-12-17 00:53:30 +0100

    ff

--- flip.c
+++ flip.c
@@ -90,7 +90,7 @@ int main (void)
     }
 
     int *       m_parameter;
-    pthread_t   thread_id[NROF_PIECES];
+    pthread_t   thread_id[NROF_THREADS];
     // loop through every possible (m)ultiple from
     // 2 and create a flipping thread for it
     for (size_t m = 2; m < NROF_PIECES; m++) 
@@ -98,10 +98,9 @@ int main (void)
         m_parameter =  malloc (sizeof (int));
         *m_parameter = m;
         printf ("%lx: starting thread ...\n", pthread_self());
-        pthread_create (&thread_id[m-2], NULL, flip_thread, m_parameter);
+        pthread_create (&thread_id[0], NULL, flip_thread, m_parameter);
         
-        // wait for the thread, and we are interested in the return value
-        //pthread_join (thread_id, NULL);
+         pthread_join (thread_id[0], NULL);
 
     }
     pthread_join (thread_id[0], NULL);
