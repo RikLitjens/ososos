5714016ecfcc7e60f1d7e61af9f444efab5b3a8a
Litjens <r.litjens@student.tue.nl>
2019-12-17 02:17:28 +0100

    asfasfasfas

--- flip.c
+++ flip.c
@@ -99,11 +99,11 @@ int main (void)
         m_parameter =  malloc (sizeof (int));
         *m_parameter = m;
         printf ("%lx: starting thread ...\n", pthread_self());
-        pthread_create (&thread_id[(m - 2) % 10 ], NULL, flip_thread, m_parameter);
+        pthread_create (&thread_id[0], NULL, flip_thread, m_parameter);
         
         // wait for the thread
 
-        pthread_join (thread_id[(m - 2) % 10], NULL);
+        pthread_join (thread_id[0], NULL);
 
     }
     
